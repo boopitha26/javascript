d88d9b50adf0171713e4d8f8d7fae24d4eebef4b
boopi26 <24bct110@kgcas.com>
2025-07-12 10:51:48 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
+var number=12;
+var name=`js`;
+var iscloudy=true;
+console.log(number+" "+name+" "+iscloudy);
+document.write(number);
+document.write(name);
+document.write(iscloudy);
+//task2
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+//task3
+var number1=parseInt(prompt("enter the first value"));
+var number2=parseInt(prompt("enter the second value"));
+alert(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
