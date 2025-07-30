d0c1bdf04146a98fc0c949b36527de6ab17d211b
boopi26 <24bct110@kgcas.com>
2025-07-30 15:06:04 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,37 @@
+let employee=[];
+function addemp(id,name,salary=true){
+    employee.push({id,name,salary});
+    console.log("employee added",employee);
+}
+addemp(1,"anu",2000)
+addemp(2,"abi",3000)
+addemp(3,"suba",4000)
+//to update emp
+function updateemp(id,newdetail){
+    const emp = employee.find(e => e.id ===id)
+    if(emp){
+        Object.assign(emp,newdetail);
+        console.log("updated",employee)
+    }else{
+        console.log("employee not found")
+    }
+}
+updateemp(3,{name:"boopi"});
+//to remove emp
+function removeemp(id){
+    const index = employee.findIndex((e)=>e.id===id);
+    if(index !==-1){
+        employee.splice(index,id)
+        console.log("employee removed",employee)
+    }
+    else{
+        console.log("employee not found");
+    }
+    }
+    removeemp(1)
+    //calculate total salary
+    function totalsalary(){
+        const total =employee.reduce((sum,emp) =>sumemp.salary,0);
+        console.log("total salary is",total);
+    }
+    totalsalary()
